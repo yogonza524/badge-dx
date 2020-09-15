89d54ed70ae8738554c87251e05c78675a3169c1
Gonzalo Mendoza <gonzalo.mendoza@globant.com>
2020-09-15 18:52:38 -0300

    Fuck

--- src/main/java/com/badge/dx/port/BadgeController.java
+++ src/main/java/com/badge/dx/port/BadgeController.java
@@ -4,6 +4,7 @@ import com.badge.dx.business.BadgeStoreService;
 import com.badge.dx.port.incoming.IncomingRequest;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class BadgeController {
@@ -31,9 +30,10 @@ public class BadgeController {
 
   @GetMapping(value = "/{project}/{repo}/{badgeName}", produces = "image/svg+xml;charset=utf-8")
   public ResponseEntity find(
-      @PathVariable String project, @PathVariable String repo, @PathVariable String badgeName,
-        HttpServletResponse httpServletResponse
-      )
+      @PathVariable String project,
+      @PathVariable String repo,
+      @PathVariable String badgeName,
+      HttpServletResponse httpServletResponse)
       throws IOException, GitAPIException {
     httpServletResponse.setHeader("Cache-Control", "no-cache");
     return ResponseEntity.ok(badgeStoreService.find(project, repo, badgeName));
