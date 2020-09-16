7409a3ced6b609ef50aeb03aa45d6754311a40c9
Gonzalo Mendoza <gonzalo.mendoza@globant.com>
2020-09-16 12:39:48 -0300

    Logging endpoints

--- src/main/java/com/badge/dx/port/BadgeController.java
+++ src/main/java/com/badge/dx/port/BadgeController.java
@@ -5,6 +5,8 @@ import com.badge.dx.business.CamoEraserService;
 import com.badge.dx.port.incoming.IncomingRequest;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class BadgeController {
 
@@ -27,6 +30,7 @@ public class BadgeController {
   @PostMapping("/create")
   public ResponseEntity create(@RequestBody IncomingRequest request)
       throws IOException, ExecutionException, InterruptedException, GitAPIException {
+    if (log.isInfoEnabled()) log.info("request=" + request);
     return ResponseEntity.ok(badgeStoreService.generateBadge(request));
   }
 
@@ -34,6 +38,7 @@ public class BadgeController {
   public ResponseEntity find(
       @PathVariable String project, @PathVariable String repo, @PathVariable String badgeName)
       throws IOException, GitAPIException {
+    if (log.isInfoEnabled()) log.info("repo=" + project + "/" + repo + ", badgeName=" + badgeName);
     return ResponseEntity.ok(badgeStoreService.find(project, repo, badgeName));
   }
 }
